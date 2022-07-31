db7230b60d9aac6c8eb253fe6f073715def14e67
Victoria-06 <ladyvic001@gmail.com>
2022-07-31 15:01:04 +0100

    edited

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\user;
+use App\Models\User;
 
 
 class UserController extends Controller
 {
     public function createuser(Request $request)
     {
-        $user = new users();
+        $user = new User();
 
         $user->name = $request->input('name');
         $user->email = $request->input('email');
@@ -28,7 +28,7 @@ class UserController extends Controller
 
     }
     public function EditUser(Request $request, $id){
-        $user = users::find($id);
+        $user = User::find($id);
         // $user->name = $request->input('name');
         // $user->email = $request->input('email');
         // $user->phone = $request->input('phone');
@@ -38,24 +38,24 @@ class UserController extends Controller
     }
     public function destroy($id)
     {
-        $user = users::find($id);
+        $user = User::find($id);
         $user->destroy($id);
         return redirect('/Users');
     }
     public function GetUser(){
-        $users = users::all();
+        $users = User::all();
         return view ('Users',['users'=>$users]);
         
     }
     public function welcome(){
-        $users = users::all();
+        $users = User::all();
         return view ('Users',['users'=>$users]);
         
     }
     public function update(Request $request,$id)
 
     {
-        $user = users::find($id);
+        $user = User::find($id);
         $input = $request->all();
         $user->update($input);
         return redirect('/Users')->with('flash_message', 'user Updated!'); 
